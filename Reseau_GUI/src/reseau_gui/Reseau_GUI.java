24ab65a42c912397fb7b92071da890fe5692bfa1
Boris FOURNIER <Boris.FOURNIER@Digital-Grenoble.local>
2017-03-21 17:54:31 +0100

    swing

---
+++ Reseau_GUI/src/reseau_gui/Reseau_GUI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reseau_gui;
+
+/**
+ *
+ * @author boris.fournier
+ */
+public class Reseau_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fenetres1 f = new Fenetres1();
+        f.setVisible(true);
+    }
+    
+}
